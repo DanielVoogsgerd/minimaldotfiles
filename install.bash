547762a5988252690bb5a21ccb538e4a3a6e433a
Daniel Voogsgerd <daniel@voogsgerd.nl>
2024-09-11 21:47:26 +0200

    feat: Download config files

--- install.bash
+++ install.bash
@@ -1,6 +1,31 @@
-# VIM
 
-# TODO: Download vim config
+function download_file() {
+    if [ -z "$1" ]; then
+        echo "download_file() needs a source parameter"
+        exit 1
+    fi
+
+    if [ -z "$2" ]; then
+        echo "download_file() needs a source parameter"
+        exit 1
+    fi
+
+    destination_dir="$(dirname "$2")"
+
+    if ! [ -e "$destination_dir" ]; then
+        mkdir "$destination_dir"
+    fi
+
+    if ! [ -d "$destination_dir" ]; then
+        echo "Destination dir is not a valid directory"
+        exit 2
+    fi
+
+    wget -O "$2" "https://raw.githubusercontent.com/DanielVoogsgerd/minimaldotfiles/main/$1"
+}
+
+# VIM
+download_file vim .vimrc
 
 # Commentary plugin
 mkdir -p ~/.vim/pack/tpope/start
@@ -18,19 +43,21 @@ git clone https://tpope.io/vim/surround.git ~/.vim/pack/tpope/start/surround
 git clone https://tpope.io/vim/repeat.git ~/.vim/pack/tpope/start/repeat
 
 # TMUX
+download_file tmux.conf .tmux.conf
 
-# TODO: Download tmux config
+tmux_plugin_path="$HOME/.config/tmux/plugins"
 
-
-tmux_plugin_path=$HOME/.config/tmux/plugins
-
-mkdir -p ~/.config/tmux/plugins/catppuccin
-git clone https://github.com/catppuccin/tmux.git ${tmux_plugin_path}/catppuccin
-git clone https://github.com/tmux-plugins/tmux-sensible ${tmux_plugin_path}/plugins/tmux-sensible
-git clone https://github.com/tmux-plugins/tmux-pain-control ${tmux_plugin_path}/tmux-pain-control
+mkdir -p "${tmux_plugin_path}"
+git clone https://github.com/catppuccin/tmux.git "${tmux_plugin_path}/catppuccin"
+git clone https://github.com/tmux-plugins/tmux-sensible "${tmux_plugin_path}/plugins/tmux-sensible"
+git clone https://github.com/tmux-plugins/tmux-pain-control "${tmux_plugin_path}/tmux-pain-control"
 
 
 # Bash
+download_file bashrc .bashrc_custom
+if [ -z "$CUSTOM_BASHRC_INSTALLED" ]; then
+    echo "source $HOME/.bashrc_custom" >> .bashrc
+fi
 
 # TODO: Add hook for custom bash config
 # TODO: Custom back config
